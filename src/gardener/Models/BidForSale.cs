04965b5db95e0457d99ffb5c8ba16c149d42f64a
Sergey <32011415+leguha@users.noreply.github.com>
2019-05-28 17:27:03 +0300

    Добавлена отправка формы на продожу места.

---
+++ src/gardener/Models/BidForSale.cs
@@ -0,0 +1,30 @@
+﻿namespace gardener.Models
+{
+	public class BidForSale
+	{
+		public BidForSale(string placeNumber, string name, string phoneNumber)
+		{
+			PlaceNumber = placeNumber;
+			Name = name;
+			PhoneNumber = phoneNumber;
+		}
+
+		public string PlaceNumber
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public string PhoneNumber
+		{
+			get;
+			set;
+		}
+	}
+}
--- src/gardener/ViewModels/FormAppBuyViewModel.cs
+++ src/gardener/ViewModels/FormAppBuyViewModel.cs
@@ -1,32 +1,111 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using gardener.Models;
 using gardener.Views.ListView;
+using Newtonsoft.Json;
+using Plugin.Connectivity;
 
 namespace gardener.ViewModels
 {
 	/// <summary>
-	/// Представляет ViewModel для <see cref="FormAppBuyPage"/>
+	/// Представляет ViewModel для <see cref="FormAppBuyPage" />
 	/// </summary>
-	public class FormAppBuyViewModel :BaseViewModel
+	public class FormAppBuyViewModel : BaseViewModel
 	{
+		#region Data
+		#region Fields
+		private string _name;
+		private string _phoneNumber;
+
+		private string _placeNumber;
+		#endregion
+		#endregion
+
+		#region .ctor
 		public FormAppBuyViewModel()
 		{
 			Title = "Форма заявки покупки";
 			SendFormCommand = new RelayCommand(x =>
-			{
-				SendForm();
-			}, x => true);
+											   {
+												   ExecuteSendFormCommand();
+											   },
+											   x => true);
 		}
+		#endregion
 
+		#region Properties
 		public RelayCommand SendFormCommand
 		{
 			get;
 		}
 
-		private void SendForm()
+		public string Name
 		{
-			//var client = new PostClient();
-			//client
-
+			get => _name;
+			set => SetProperty(ref _name, value);
 		}
+
+		public string PhoneNumber
+		{
+			get => _phoneNumber;
+			set => SetProperty(ref _phoneNumber, value);
+		}
+
+		public string PlaceNumber
+		{
+			get => _placeNumber;
+			set => SetProperty(ref _placeNumber, value);
+		}
+		#endregion
+
+		#region Private
+		private async void ExecuteSendFormCommand()
+		{
+			IsBusy = true;
+			if (CrossConnectivity.Current.IsConnected)
+			{
+				await Task.Run(() =>
+				{
+					SendForm("http://tc.itmit-studio.ru/api/bid-for-sale");
+				});
+			}
+			else
+			{
+				Title = "Ожидание сети";
+			}
+		}
+
+		private async void SendForm(string url)
+		{
+			var bidForSale = new BidForSale(PlaceNumber, Name, PhoneNumber);
+			var client = new HttpClient();
+			var request = new HttpRequestMessage(HttpMethod.Post, new Uri(url))
+			{
+				Content = new StringContent(JsonConvert.SerializeObject(bidForSale))
+			};
+
+			var parameters = new Dictionary<string, string>
+			{
+				{
+					"data", JsonConvert.SerializeObject(bidForSale)
+				}
+			};
+			var encodedContent = new FormUrlEncodedContent(parameters);
+
+			var response = await client.PostAsync(url, encodedContent);
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				// TODO: Дописать действия в случае успешной и отправки формы.
+			}
+			else
+			{
+				// TODO: Дописать действия в случае отправки формы с ошибкой.
+			}
+			IsBusy = false;
+		}
+		#endregion
 	}
 }
