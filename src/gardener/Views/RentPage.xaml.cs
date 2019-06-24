64dad5f97458cc1907836e27bad2d78a115ded8c
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-06-24 18:00:26 +0300

    Новости рынка

--- src/gardener/Views/RentPage.xaml.cs
+++ src/gardener/Views/RentPage.xaml.cs
@@ -4,13 +4,15 @@ using Xamarin.Forms.Xaml;
 namespace gardener.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class RentPage : TabbedPage
+	public partial class RentPage : ContentPage
 	{
 		#region .ctor
 		public RentPage()
 		{
 			InitializeComponent();
-		}
+
+            //BindingContext = new RentSaleViewModel(Navigation);
+        }
 		#endregion
 	}
 }
