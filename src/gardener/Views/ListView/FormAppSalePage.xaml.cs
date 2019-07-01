977ff5812506d1f8fdbc757f27ad77f51eb3b76d
Sergey <32011415+leguha@users.noreply.github.com>
2019-07-01 15:40:34 +0300

    Добавлены алерты, которые выводится при отправке формы

--- src/gardener/Views/ListView/FormAppSalePage.xaml.cs
+++ src/gardener/Views/ListView/FormAppSalePage.xaml.cs
@@ -9,13 +9,27 @@ namespace gardener.Views.ListView
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class FormAppSalePage : ContentPage
 	{
+		private FormAppSaleViewModel _viewModel;
+
 		#region .ctor
 		public FormAppSalePage(Block block)
 		{
 			InitializeComponent();
-
-			BindingContext = new FormAppSaleViewModel(block, new Uri("http://tc.itmit-studio.ru/api/bidForSale"), "Форма заявки на аренду помещения");
+			_viewModel = new FormAppSaleViewModel(block, new Uri("http://tc.itmit-studio.ru/api/bidForSale"), "Форма заявки на аренду помещения");
+			BindingContext = _viewModel;
 		}
 		#endregion
+
+		private void OnSendForm(bool isSuccess)
+		{
+			if (isSuccess)
+			{
+				DisplayAlert("Внимание", "Форма успешно отправлена", "Ok");
+			}
+			else
+			{
+				DisplayAlert("Внимание", _viewModel.GetLastError(), "Ok");
+			}
+		}
 	}
 }
