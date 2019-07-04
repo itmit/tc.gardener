bdf4cfbc6c1a36248538a76ff82f7a1fd6008c61
Sergey <32011415+leguha@users.noreply.github.com>
2019-07-04 15:50:08 +0300

    Переделаны курсы валют

---
+++ src/gardener/Models/Course.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace gardener.Models
+{
+	public class Course
+	{
+		public string Date
+		{
+			get;
+			set;
+		}
+
+		public string PreviousDate
+		{
+			get;
+			set;
+		}
+
+		public string PreviousUrl
+		{
+			get;
+			set;
+		}
+
+		public string Timestamp
+		{
+			get;
+			set;
+		}
+
+		[JsonProperty("Valute")]
+		public Valute Values
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ src/gardener/Models/Currency.cs
@@ -0,0 +1,49 @@
+﻿namespace gardener.Models
+{
+	public class Currency
+	{
+		public string Id
+		{
+			get;
+			set;
+		}
+
+		public int NumCode
+		{
+			get;
+			set;
+		}
+
+		public string CharCode
+		{
+			get;
+			set;
+		}
+
+		public int Nominal
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public float Value
+		{
+			get;
+			set;
+		}
+
+		public float Previous
+		{
+			get;
+			set;
+		}
+
+		public string Image => CharCode + ".png";
+	}
+}
