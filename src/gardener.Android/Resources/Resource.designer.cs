a02fddfb4c6eb3575f5b4ec1c6a040f74fbbc45a
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-07-29 14:37:19 +0300

    Меню. Флажки мультиязычной версии.

--- src/gardener.Android/Resources/Resource.designer.cs
+++ src/gardener.Android/Resources/Resource.designer.cs
@@ -5778,26 +5778,26 @@ namespace gardener.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020154
-			public const int avd_hide_password_1 = 2130837844;
+			// aapt resource value: 0x7f020157
+			public const int avd_hide_password_1 = 2130837847;
 			
-			// aapt resource value: 0x7f020155
-			public const int avd_hide_password_2 = 2130837845;
+			// aapt resource value: 0x7f020158
+			public const int avd_hide_password_2 = 2130837848;
 			
-			// aapt resource value: 0x7f020156
-			public const int avd_hide_password_3 = 2130837846;
+			// aapt resource value: 0x7f020159
+			public const int avd_hide_password_3 = 2130837849;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020157
-			public const int avd_show_password_1 = 2130837847;
+			// aapt resource value: 0x7f02015a
+			public const int avd_show_password_1 = 2130837850;
 			
-			// aapt resource value: 0x7f020158
-			public const int avd_show_password_2 = 2130837848;
+			// aapt resource value: 0x7f02015b
+			public const int avd_show_password_2 = 2130837851;
 			
-			// aapt resource value: 0x7f020159
-			public const int avd_show_password_3 = 2130837849;
+			// aapt resource value: 0x7f02015c
+			public const int avd_show_password_3 = 2130837852;
 			
 			// aapt resource value: 0x7f020056
 			public const int CNY = 2130837590;
@@ -6495,11 +6495,11 @@ namespace gardener.Droid
 			// aapt resource value: 0x7f02013d
 			public const int notification_icon_background = 2130837821;
 			
-			// aapt resource value: 0x7f020152
-			public const int notification_template_icon_bg = 2130837842;
+			// aapt resource value: 0x7f020155
+			public const int notification_template_icon_bg = 2130837845;
 			
-			// aapt resource value: 0x7f020153
-			public const int notification_template_icon_low_bg = 2130837843;
+			// aapt resource value: 0x7f020156
+			public const int notification_template_icon_low_bg = 2130837846;
 			
 			// aapt resource value: 0x7f02013e
 			public const int notification_tile_bg = 2130837822;
@@ -6517,49 +6517,58 @@ namespace gardener.Droid
 			public const int pict_1 = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int svadebnaya_galereya_SALYuT = 2130837827;
+			public const int rus = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int tc2 = 2130837828;
+			public const int svadebnaya_galereya_SALYuT = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int tc3 = 2130837829;
+			public const int tc2 = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int tc4 = 2130837830;
+			public const int tc3 = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int tc5 = 2130837831;
+			public const int tc4 = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int tc6 = 2130837832;
+			public const int tc5 = 2130837832;
 			
 			// aapt resource value: 0x7f020149
-			public const int tc7 = 2130837833;
+			public const int tc6 = 2130837833;
 			
 			// aapt resource value: 0x7f02014a
-			public const int tc9 = 2130837834;
+			public const int tc7 = 2130837834;
 			
 			// aapt resource value: 0x7f02014b
-			public const int tooltip_frame_dark = 2130837835;
+			public const int tc9 = 2130837835;
 			
 			// aapt resource value: 0x7f02014c
-			public const int tooltip_frame_light = 2130837836;
+			public const int tooltip_frame_dark = 2130837836;
 			
 			// aapt resource value: 0x7f02014d
-			public const int tts_sadovod = 2130837837;
+			public const int tooltip_frame_light = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int UAH = 2130837838;
+			public const int tts_sadovod = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int USD = 2130837839;
+			public const int UAH = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int veschevye_ryady = 2130837840;
+			public const int USD = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int ViewCellBackground = 2130837841;
+			public const int veschevye_ryady = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int vietnam = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int ViewCellBackground = 2130837843;
+			
+			// aapt resource value: 0x7f020154
+			public const int zh = 2130837844;
 			
 			static Drawable()
 			{
