ff741e70f5cf6da2991d8952a9674550756f4881
leguha <trulleguha@gmail.com>
2019-08-21 09:03:16 +0300

    Добавлены недостающие картинки

---
+++ src/gardener.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel;
+using gardener;
+using UIKit;  
+using Xamarin.Forms;  
+using Xamarin.Forms.Platform.iOS;
+using XamarinEntry.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(CustomEntryRenderer))]  
+    namespace XamarinEntry.iOS.Renderers
+{  
+        public class CustomEntryRenderer: EntryRenderer  
+        {  
+            protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)  
+            {  
+                base.OnElementPropertyChanged(sender, e);
+
+                Control.BackgroundColor = UIColor.FromRGB(255, 236, 209);
+            }  
+        }  
+    }  
