244073f8cc4704248dd4fac6c8ee2d628206e17c
leguha <trulleguha@gmail.com>
2019-08-21 11:46:21 +0300

    иконки и настройки

--- src/gardener.iOS/Renderers/ZoomScrollViewRenderer.cs
+++ src/gardener.iOS/Renderers/ZoomScrollViewRenderer.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.Linq;
+using gardener.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(ScrollView), typeof(ZoomScrollViewRenderer))]
 namespace gardener.iOS.Renderers
 {
-    public class ZoomScrollViewRenderer
+    public class ZoomScrollViewRenderer : ScrollViewRenderer
     {
-        public ZoomScrollViewRenderer()
+        // bool zoomEnabled = false;
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
         {
+            base.OnElementChanged(e);
+            MaximumZoomScale = 3f;
+            MinimumZoomScale = 1.0f;
+
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            if (Subviews.Length > 0)
+            {
+                ViewForZoomingInScrollView += GetViewForZooming;
+            }
+            else
+            {
+                ViewForZoomingInScrollView -= GetViewForZooming;
+            }
+
+        }
+
+        public UIView GetViewForZooming(UIScrollView sv)
+        {
+            return Subviews.FirstOrDefault();
         }
     }
 }
